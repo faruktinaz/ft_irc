4e8fee09b311d8062435f147bb0329efbe8f4e4c
faruktinaz <faruktinazz@icloud.com>
2024-03-31 23:38:10 +0300

    added functions to header file & macros & vectors

--- Server/Server.hpp
+++ Server/Server.hpp
@@ -11,14 +11,48 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sstream>
 
-#define SOCKET_ERROR -1
+# define SOCKET_ERROR -1
+# define PASS "PASS"
+# define JOIN "JOIN"
+# define USER "USER"
+# define NICK "NICK"
+
+// void Harl::complain(std::string level)
+// {
+//     int j;
+// 	fpoint fp[4] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+// 	std::string type[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+//     for (j = 0; j < 4 ; ++j) {
+//         if (type[j] == level)
+//             break;
+//     }
+
+//     switch (j)
+//     {
+//         case 0:
+//             (this->*fp[0])();
+//         case 1:
+//             (this->*fp[1])();
+//         case 2:
+//             (this->*fp[2])();
+//         case 3:
+//             (this->*fp[3])();
+//             break;
+//         default:
+//             std::cout << "Invalid arguman!!";
+//     }
+// }
+
+
 
 class Server
 {
     private:
 	    struct sockaddr_in			server_address;
-
+		typedef int(Server::*fpoint)();
         std::vector<Channel>		channels;
         std::vector<Client>			clients;
         std::vector<std::string>	commands;
@@ -34,13 +68,15 @@ class Server
     public:
 		Server(int port, std::string arg_pass);
 		std::vector<Channel> 		getChannels();
-		void						initilizeServer();
-		std::vector<Client> 		getClients();
-		int 						perr(std::string err, int sockfd);
 		std::vector<std::string>	getCommands();
-		int							checkCommands(Server &server, std::string buffer);
-		int							getAcceptFd();
+		std::vector<Client> 		getClients();
+
+		void						executeCommand();
 		int							getServerFd();
+		int							getAcceptFd();
 		int							getPort();
+		void						initilizeServer();
+		int 						perr(std::string err, int sockfd);
+		void						checkCommands(Server &server, std::string buffer, int socket);
 		~Server();
 };
