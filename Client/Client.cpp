53d217ac78c39534f994c7f08572d708dc32efce
faruktinaz <faruktinazz@icloud.com>
2024-04-01 22:53:58 +0300

    added setter functions to client

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -12,4 +12,12 @@ std::string Client::getNickName(){ return this->nick_name;}
 
 std::string Client::getPass(){ return this->pass; }
 
+
+
+void Client::setSocket(int sockfd) {this->socket = sockfd;}
+void Client::setUserName(std::string user_name) {this->user_name = user_name;}
+void Client::setNickName(std::string nick_name) {this->nick_name = nick_name;}
+void Client::setPass(std::string pass) {this->pass = pass;}
+
+
 Client::~Client() { }
