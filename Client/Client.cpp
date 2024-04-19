fc911fecee7be46ba7de0e17940f96897ea81f2a
faruktinaz <faruktinazz@icloud.com>
2024-04-19 19:33:37 +0300

    client update

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,8 +1,19 @@
 #include "Client.hpp"
 
+Client::Client()
+{
+    this->pass = "";
+    this->nick_name = "";
+    this->user_name = "";
+	this->ip = "";
+	this->socket = -1;
+    this->logged_in = 0;
+}
 
-Client::Client() { }
+
 int         Client::getSocket() { return this->socket; }
+
+int			Client::getLoggedIn() { return this->logged_in; }
 
 std::string Client::getIp(){ return this->ip; }
 
@@ -12,12 +23,18 @@ std::string Client::getNickName(){ return this->nick_name;}
 
 std::string Client::getPass(){ return this->pass; }
 
+void        Client::setSocket(int sockfd) { this->socket = sockfd; }
 
+void        Client::setUserName(std::string user_name) { this->user_name = user_name; }
 
-void Client::setSocket(int sockfd) {this->socket = sockfd;}
-void Client::setUserName(std::string user_name) {this->user_name = user_name;}
-void Client::setNickName(std::string nick_name) {this->nick_name = nick_name;}
-void Client::setPass(std::string pass) {this->pass = pass;}
+void        Client::setNickName(std::string nick_name) { this->nick_name = nick_name; }
 
+void        Client::setPass(std::string pass) { this->pass = pass; }
+
+void        Client::setLoggedIn(int sign) { this->logged_in = sign; }
+
+void		Client::setIp(std::string ip) { this->ip = ip;}
+
+void        Client::print(std::string str) { send(this->socket, str.c_str(), str.size(), 0); }
 
 Client::~Client() { }
--- Client/Client.hpp
+++ Client/Client.hpp
@@ -1,10 +1,12 @@
 #pragma once
 #include "iostream"
+#include <sys/socket.h>
 
 class Client
 {
     private:
         int         socket;
+        int         logged_in;
         std::string user_name;
         std::string nick_name;
         std::string pass;
@@ -12,15 +14,19 @@ class Client
     public:
 		Client();
         int         getSocket();
+        int         getLoggedIn();
         std::string getUserName();
         std::string getNickName();
         std::string getPass();
         std::string getIp();
 
-		void setSocket(int sockfd);
-        void setUserName(std::string user_name);
-        void setNickName(std::string nick_name);
-        void setPass(std::string pass);
+		void        print(std::string str);
+		void        setSocket(int sockfd);
+        void        setLoggedIn(int sign);
+        void        setUserName(std::string user_name);
+        void        setNickName(std::string nick_name);
+        void        setPass(std::string pass);
+		void		setIp(std::string ip);
 
 
 		~Client();
