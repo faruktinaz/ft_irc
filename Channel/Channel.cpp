d4adfa1bef72594eb47ac3d53ee45607510af4cb
faruktinaz <faruktinazz@icloud.com>
2024-04-19 19:33:15 +0300

    update channel

--- Channel/Channel.cpp
+++ Channel/Channel.cpp
@@ -1,1 +1,14 @@
 #include "Channel.hpp"
+
+
+Channel::Channel(std::string name) { this->channelName = name; }
+
+std::vector<Client> Channel::getClients() { return this->clients; }
+
+std::string         Channel::getChannelName() { return this->channelName; }
+
+void				Channel::addClient(Client client) { clients.push_back(client); }
+
+void                Channel::setChannelAdmin(std::string adminName) { this->ChannelAdmin = adminName; }
+
+Channel::~Channel() { }
--- Channel/Channel.hpp
+++ Channel/Channel.hpp
@@ -1,9 +1,22 @@
 #pragma once
 
+#include "../Client/Client.hpp"
+#include <iostream>
+#include <vector>
+
 class Channel
 {
     private:
-		
+        std::vector<Client> clients;
+        std::string channelName;
+        std::string ChannelAdmin;
+
     public:
+        Channel(std::string name);
+        std::vector<Client> getClients();
+        std::string         getChannelName();
+        void                setChannelAdmin(std::string adminName);
+		void				addClient(Client client);
+        ~Channel();
 
 };
