7bcdfba03f43183f1a5dc0cd9de4a7ea1ec438f1
faruktinaz <faruktinazz@icloud.com>
2024-04-28 03:21:55 +0300

    update user

--- Commands/User.cpp
+++ Commands/User.cpp
@@ -8,8 +8,6 @@ void Server::User(int index, int id)
         clients[id].setUserName(commands[index + 1]);
         clients[id].setIp(commands[index + 3]);
 	}
-	else if(commands[index + 1].empty() == 0 && commands[index + 3].empty() == 1)
-        clients[id].setUserName(commands[index + 1]);
     else
-		std::cout << "The format is 'USER username'" << std::endl;
+		std::cout << "The format is 'USER username 0 realname'" << std::endl;
 }
